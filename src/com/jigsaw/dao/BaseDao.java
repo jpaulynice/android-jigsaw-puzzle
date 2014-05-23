b298f2c0b92d6d4ff8f8c14d7e1edba1c58d26da
julesbond007 <jay.paulynice@gmail.com>
2014-05-23 16:22:14 -0400

    changes

--- src/com/jigsaw/dao/BaseDao.java
+++
@@ -1,7 +0,0 @@
-package com.jigsaw.dao;
-
-/**
- * Created by Jules Jay Paulynice on 7/8/13.
- */
-public interface BaseDao {
-}
--- src/com/jigsaw/service/Drawing.java
+++
@@ -1,7 +0,0 @@
-package com.jigsaw.service;
-
-/**
- * Created by Jules Jay Paulynice on 7/8/13.
- */
-public interface Drawing {
-}
