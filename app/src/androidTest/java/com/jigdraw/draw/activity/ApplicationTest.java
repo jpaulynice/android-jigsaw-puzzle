d7e0876e168912faf6f53f7513bf3ef7ba98362b
julesbond007 <jay.paulynice@gmail.com>
2015-01-25 20:17:27 -0500

    changes

---
+++ app/src/androidTest/java/com/jigdraw/draw/activity/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.jigdraw.draw.activity;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Test class
+ *
+ * @author Jay Paulynice
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
