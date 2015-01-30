984840d4b6b2ff609e24888099680fc6b762e393
julesbond007 <jay.paulynice@gmail.com>
2015-01-29 21:49:59 -0500

    create own data provider

--- app/src/main/java/com/jigdraw/draw/service/ImageService.java
+++ app/src/main/java/com/jigdraw/draw/service/ImageService.java
@@ -8,34 +8,12 @@ import com.jigdraw.draw.model.ImageEntity;
  * @author Jay Paulynice
  */
 public interface ImageService {
-    /**
-     * Save the given image entity
-     *
-     * @param entity the entity to save
-     * @return id of the created entity
-     */
+
     public long insert(ImageEntity entity);
 
-    /**
-     * Get image entity from db matching id
-     *
-     * @param id unique id for the image
-     * @return image entity
-     */
     public ImageEntity query(long id);
 
-    /**
-     * Update given image entity
-     *
-     * @param entity image to update
-     * @return update image
-     */
     public boolean update(ImageEntity entity);
 
-    /**
-     * Delete the image entity with the given id
-     *
-     * @param id the id of the image to delete
-     */
     public boolean delete(long id);
 }
