1297473c0ca849a3fe71bc939359692c89eed2cc
Jay Paulynice <julesbond007@users.noreply.github.com>
2017-09-06 18:01:07 -0400

    testing

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "ASSEMBLE ANDROIDTEST..."
+./gradlew assembleAndroidTest
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo "error on assembleTest, exit code: "$retval
+    exit $retval
+fi
+./gradlew clean
+echo "DONE WITH ASSEMBLING ANDROIDTEST"
+echo "BUILDING..."
+./gradlew build
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo "error on building, exit code: "$retval
+    exit $retval
+fi
+./gradlew clean
+echo "DONE WITH BUILDING..."
