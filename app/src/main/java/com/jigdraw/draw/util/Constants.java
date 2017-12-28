06c7f93e11a42a111beefb73dbf384c6bf861a61
Jay Paulynice <julesbond007@users.noreply.github.com>
2017-12-28 11:28:52 -0500

    Update Constants.java

--- app/src/main/java/com/jigdraw/draw/util/Constants.java
+++ app/src/main/java/com/jigdraw/draw/util/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final int MOVE_DURATION = 300;
     public static final int SMOOTH_SCROLL_AMOUNT_AT_EDGE = 8;
     
-    priivate Constants(){}
+    private Constants(){}
 }
