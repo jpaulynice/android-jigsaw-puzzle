a7fe76fdea66dce23fecd94bcf17be15bd9336fc
julesbond007 <jay.paulynice@gmail.com>
2018-11-29 12:06:56 -0500

    committing to fix tests later.

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "ASSEMBLE ANDROIDTEST..."
-./gradlew assembleAndroidTest
+../gradlew assembleAndroidTest
 retval=$?
 if [ $retval -ne 0 ]; then
     echo "error on assembleTest, exit code: "$retval
     exit $retval
 fi
-./gradlew clean
+../gradlew clean
 echo "DONE WITH ASSEMBLING ANDROIDTEST"
 echo "BUILDING..."
-./gradlew build
+../gradlew build
 retval=$?
 if [ $retval -ne 0 ]; then
     echo "error on building, exit code: "$retval
     exit $retval
 fi
-./gradlew clean
+../gradlew clean
 echo "DONE WITH BUILDING..."
